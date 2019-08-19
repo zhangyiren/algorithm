a956c26d4f7d784942c79233947751504197e29f
zhangyiren <alex_yiren@163.com>
2019-08-19 23:23:26 +0800

    merge sort

--- php/merge_sort.php
+++ php/merge_sort.php
@@ -49,18 +49,17 @@ function merge($array1,$array2)
     	$array = [706,100,188,9,11,25,133,452,364,5889,293,607,365,8633,555,18,66,78,15,43,67,96,155,387,972,1002,74,38,633,801];
 	$arrayLen = count($array);
 	for($i = 0 ; $i < $arrayLen ; $i++) {
-        $array1[] = $array[$i];
+        	$array1[] = $array[$i];
 		if(count($array1) == 2) {
-            // swap element make it sequential
-            if($array1[0] > $array1[1]) {
-                $tmp = $array1[0];
+            		// swap element make it sequential
+            		if($array1[0] > $array1[1]) {
+                		$tmp = $array1[0];
 				$array1[0] = $array1[1];
 				$array1[1] = $tmp;
-			}
-            $array2 = merge($array1,$array2);
+           	 	}
+            	 	$array2 = merge($array1,$array2);
 			$array1 = [];
 		}
-
 	}
 	// maybe some elements left, merge it too
 	$array2 = merge($array1,$array2);
